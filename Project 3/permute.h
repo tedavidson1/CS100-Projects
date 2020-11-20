63bb3ac0fd22ab3eddec73394194ab628c6ade09
tedavidson1 <48767632+tedavidson1@users.noreply.github.com>
2020-11-20 16:29:23 -0600

    Create permute.h

---
+++ Project 3/permute.h
@@ -0,0 +1,16 @@
+#ifndef _PERMUTE_H_
+#define _PERMUTE_H_
+
+// Give a permutation of n integers saved in the array x,
+// find the next permutation in lexicographical order, and
+// save it into the array x.
+// Return 1 if next permutation is found. Otherwise, return 0.
+int findNext(int n, int x[]);
+
+// Print out the array in one line
+void printArray(int n, int a[]);
+
+// Sort the array in ascending order
+void sortArray(int n, int a[]);
+
+#endif
